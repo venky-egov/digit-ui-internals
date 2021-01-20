5c9bf7aff732cbc785f9924f7df3e8e9ba5992d6
himanshu-egov <himanshu.singh@egovernments.org>
2021-01-21 00:25:04 +0530

    format

--- packages/modules/core/src/components/ChangeLanguage.js
+++ packages/modules/core/src/components/ChangeLanguage.js
@@ -4,8 +4,8 @@ import { CustomButton, Menu } from "@egovernments/digit-ui-react-components";
 import { useSelector } from "react-redux";
 
 const ChangeLanguage = (prop) => {
-  const isDropdown = prop.dropdown ||  false;
-  console.log({isDropdown});
+  const isDropdown = prop.dropdown || false;
+  console.log({ isDropdown });
   const commonState = useSelector((state) => state.common);
   const { languages, selectedLanguage, stateInfo } = commonState;
   const [selected, setselected] = useState(selectedLanguage);
@@ -15,30 +15,36 @@ const ChangeLanguage = (prop) => {
     Digit.LocalizationService.changeLanguage(language.value, stateInfo.code);
   };
   if (isDropdown) {
-    return <div>
-      <Dropdown
-        option={languages}
-        selected={languages.find((language)=>language.value === selectedLanguage)}
-        optionKey={"label"}
-        select={handleChangeLanguage}
-        freeze={true}
-        customSelector={<label className="right cp" >{languages.find((language)=>language.value === selected).label}</label>}
-      />
-    </div>
-  } else {
-    return <React.Fragment>
-      <div style={{ marginBottom: "5px" }}>Language</div>
-      <div className="language-selector">
-        {languages.map((language, index) => (
-          <div className="language-button-container" key={index}>
-            <CustomButton selected={language.value === selected} text={language.label} onClick={() => handleChangeLanguage(language)}></CustomButton>
-          </div>
-        ))}
+    return (
+      <div>
+        <Dropdown
+          option={languages}
+          selected={languages.find((language) => language.value === selectedLanguage)}
+          optionKey={"label"}
+          select={handleChangeLanguage}
+          freeze={true}
+          customSelector={<label className="right cp">{languages.find((language) => language.value === selected).label}</label>}
+        />
       </div>
-    </React.Fragment>
+    );
+  } else {
+    return (
+      <React.Fragment>
+        <div style={{ marginBottom: "5px" }}>Language</div>
+        <div className="language-selector">
+          {languages.map((language, index) => (
+            <div className="language-button-container" key={index}>
+              <CustomButton
+                selected={language.value === selected}
+                text={language.label}
+                onClick={() => handleChangeLanguage(language)}
+              ></CustomButton>
+            </div>
+          ))}
+        </div>
+      </React.Fragment>
+    );
   }
-  
-  ;
 };
 
 export default ChangeLanguage;

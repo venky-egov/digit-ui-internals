985327a7caa07e00d18239dcb2ef66fde0288896
saurabh-egov <saurabh.s@egovernments.org>
2021-01-21 19:21:07 +0530

    added QA GRO AMR user for testing

--- example/src/index.js
+++ example/src/index.js
@@ -13,6 +13,7 @@ import CITIZEN from "./userInfo/citizen.json";
 import EMPLOYEE from "./userInfo/employee.json";
 import LME from "./userInfo/lme.json";
 import GRO from "./userInfo/gro.json";
+import QAGRO from "./userInfo/qa-gro.json";
 import QACSR from "./userInfo/qa-csr.json";
 import QACT from "./userInfo/qa-citizen.json";
 import FSM_EMPLOYEE from "./userInfo/fsm-employee.json";
@@ -24,7 +25,7 @@ import { pgrCustomizations, pgrComponents } from "./pgr";
 
 initLibraries();
 
-const userInfo = { CITIZEN, EMPLOYEE, LME, GRO, QACSR, QACT, FSM_EMPLOYEE, NAWANSHAHR_QA_GRO };
+const userInfo = { CITIZEN, EMPLOYEE, LME, GRO, QACSR, QACT, QAGRO, FSM_EMPLOYEE, NAWANSHAHR_QA_GRO };
 
 const enabledModules = ["PGR"];
 const registry = new Registry({

b883a43549a6c84868e4e1c71951ce3b0c3479aa
Mahendra <mahendra.choudhary@egovernments.org>
2021-01-27 17:18:42 +0530

    add payment button

--- packages/modules/fsm/src/pages/citizen/ApplicationDetails.js
+++ packages/modules/fsm/src/pages/citizen/ApplicationDetails.js
@@ -112,9 +112,9 @@ const ApplicationDetails = () => {
         />
         <KeyNote keyValue={t("CS_APPLICATION_DETAILS_NO_OF_TRIPS")} note={application.noOfTrips} />
         <KeyNote keyValue={t("CS_APPLICATION_DETAILS_DESLUDGING_CHARGES")} note={application.desuldgingCharges || "NA"} />
-        {application.status !== "PENDING_APPL_FEE_PAYMENT" && (
-          <Link to={`/digit-ui/citizen/fsm/rate/${application.applicationNo}`}>
-            <SubmitBar label={t("CS_APPLICATION_DETAILS_RATE_US")} />
+        {application.status === "PENDING_APPL_FEE_PAYMENT" && (
+          <Link to={`/digit-ui/citizen/payment/collect/FSM.TRIP_CHARGES/${application.applicationNo}`}>
+            <SubmitBar label={t("CS_APPLICATION_DETAILS_MAKE_PAYMENT")} />
           </Link>
         )}
       </Card>

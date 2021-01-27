1eb8955f3179d111f29e53671535ab0a0a63193c
mahendra-egov <75667085+mahendra-egov@users.noreply.github.com>
2021-01-27 15:37:25 +0530

    update PAYMENT to PENDING_APPL_FEE_PAYMENT

--- packages/modules/fsm/src/pages/citizen/ApplicationDetails.js
+++ packages/modules/fsm/src/pages/citizen/ApplicationDetails.js
@@ -112,7 +112,7 @@ const ApplicationDetails = () => {
         />
         <KeyNote keyValue={t("CS_APPLICATION_DETAILS_NO_OF_TRIPS")} note={application.noOfTrips} />
         <KeyNote keyValue={t("CS_APPLICATION_DETAILS_DESLUDGING_CHARGES")} note={application.desuldgingCharges || "NA"} />
-        {application.status !== "PAYMENT" && (
+        {application.status !== "PENDING_APPL_FEE_PAYMENT" && (
           <Link to={`/digit-ui/citizen/fsm/rate/${application.applicationNo}`}>
             <SubmitBar label={t("CS_APPLICATION_DETAILS_RATE_US")} />
           </Link>

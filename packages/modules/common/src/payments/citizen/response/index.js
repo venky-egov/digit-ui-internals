f4ebadeead946c413506650046bfa54a89a8b754
Abhinav Kushwaha <abhinav.kushwaha@egovernments.org>
2021-01-28 23:30:47 +0530

    error handling in payment

--- packages/modules/common/src/payments/citizen/response/index.js
+++ packages/modules/common/src/payments/citizen/response/index.js
@@ -8,11 +8,20 @@ export const SuccessfulPayment = (props) => {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   const egId = urlParams.get("eg_pg_txnid");
-  const { isLoading, data: payments } = Digit.Hooks.usePaymentUpdate({ egId });
+  const { isLoading, data: payments, isError } = Digit.Hooks.usePaymentUpdate({ egId });
   const [printing, setPrinting] = useState(false);
 
   if (isLoading) {
     return <Loader />;
+  }
+
+  if (isError) {
+    <Card>
+      <Banner message={t("CITIZEN_FAILURE_COMMON_PAYMENT_MESSAGE")} info="" successful={false} />
+      <Link to="/digit-ui/citizen">
+        <SubmitBar label={t("CORE_COMMON_GO_TO_HOME")} />
+      </Link>
+    </Card>;
   }
 
   const paymentData = payments?.Payments[0];
@@ -33,9 +42,7 @@ export const SuccessfulPayment = (props) => {
     setPrinting(false);
   };
 
-  const bannerText = `CITIZEN_${
-    paymentData?.paymentStatus !== "DEPOSITED" ? "FAILURE" : "SUCCESS"
-  }_${paymentData?.paymentDetails[0].businessService.replace(/\./g, "_")}_PAYMENT_MESSAGE`;
+  const bannerText = `CITIZEN_SUCCESS_${paymentData?.paymentDetails[0].businessService.replace(/\./g, "_")}_PAYMENT_MESSAGE`;
 
   return (
     <Card>
@@ -43,19 +50,17 @@ export const SuccessfulPayment = (props) => {
         message={t(bannerText)}
         info={t(`${bannerText}_DETAIL`)}
         applicationNumber={paymentData?.paymentDetails[0].receiptNumber}
-        successful={paymentData?.paymentStatus !== "DEPOSITED"}
+        successful={true}
       />
-      {paymentData?.paymentStatus !== "DEPOSITED" && (
-        <React.Fragment>
-          <div className="primary-label-btn d-flex" onClick={printReciept}>
-            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
-              <path d="M0 0h24v24H0z" fill="none" />
-              <path d="M19 8H5c-1.66 0-3 1.34-3 3v6h4v4h12v-4h4v-6c0-1.66-1.34-3-3-3zm-3 11H8v-5h8v5zm3-7c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1zm-1-9H6v4h12V3z" />
-            </svg>
-            {t("COMMON_PRINT_RECEIPT")}
-          </div>
-        </React.Fragment>
-      )}
+      <React.Fragment>
+        <div className="primary-label-btn d-flex" onClick={printReciept}>
+          <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
+            <path d="M0 0h24v24H0z" fill="none" />
+            <path d="M19 8H5c-1.66 0-3 1.34-3 3v6h4v4h12v-4h4v-6c0-1.66-1.34-3-3-3zm-3 11H8v-5h8v5zm3-7c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1zm-1-9H6v4h12V3z" />
+          </svg>
+          {t("COMMON_PRINT_RECEIPT")}
+        </div>
+      </React.Fragment>
       <Link to="/digit-ui/citizen">
         <SubmitBar label={t("CORE_COMMON_GO_TO_HOME")} />
       </Link>

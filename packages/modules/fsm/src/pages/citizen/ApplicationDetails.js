0906b9c724c96264184514e81d614b5f142e7d19
ayan-egov <ayan.majumder@egovernments.org>
2021-01-28 17:27:05 +0530

    Fixed details page

--- packages/modules/fsm/src/pages/citizen/ApplicationDetails.js
+++ packages/modules/fsm/src/pages/citizen/ApplicationDetails.js
@@ -2,6 +2,7 @@ import React, { Fragment } from "react";
 import { useTranslation } from "react-i18next";
 import { Header, Card, KeyNote, SubmitBar, LinkButton, Loader } from "@egovernments/digit-ui-react-components";
 import { Link, useHistory, useParams } from "react-router-dom";
+import { getPropertySubtypeLocale } from "../../utils";
 
 const capitalize = (text) => text.substr(0, 1).toUpperCase() + text.substr(1);
 const ulbCamel = (ulb) => ulb.toLowerCase().split(" ").map(capitalize).join(" ");
@@ -98,20 +99,31 @@ const ApplicationDetails = () => {
           onClick={() => Digit.Utils.pdf.generate(getPDFData(application, tenantInfo, t))}
         />
         <KeyNote keyValue={t("CS_MY_APPLICATION_APPLICATION_NO")} note={application.applicationNo} />
-        <KeyNote keyValue={t("CS_APPLICATION_DETAILS_SERVICE_CATEGORY")} note={application.serviceCategory || "FSM"} />
-        <KeyNote keyValue={t("CS_APPLICATION_DETAILS_APPLICATION_TYPE")} note={application.applicationType || "Desludging Request"} />
+        <KeyNote keyValue={t("CS_APPLICATION_DETAILS_SERVICE_CATEGORY")} note={application.serviceCategory || t("ES_TITLE_FSM")} />
+        <KeyNote keyValue={t("CS_APPLICATION_DETAILS_APPLICATION_TYPE")} note={application.applicationType || t("CS_APPLICATION_TYPE_DESLUDGING")} />
         <KeyNote keyValue={t("CS_APPLICATION_DETAILS_STATUS")} note={application.applicationStatus} />
         <KeyNote
           keyValue={t("CS_APPLICATION_DETAILS_APPLICATION_DATE")}
           note={Digit.DateUtils.ConvertTimestampToDate(application.auditDetails.createdTime)}
         />
-        <KeyNote keyValue={t("CS_APPLICATION_DETAILS_PROPERTY_TYPE")} note={t(`PROPERTYTAX_BILLING_SLAB_${application.propertyUsage}`)} />
+        <KeyNote keyValue={t("CS_APPLICATION_DETAILS_PROPERTY_TYPE")} note={t(getPropertySubtypeLocale(application.propertyUsage))} />
+        <KeyNote keyValue={t("MYCITY_CODE_LABEL")} note={application.address.city} />
+        <KeyNote keyValue={t("CS_CREATECOMPLAINT_MOHALLA")} note={application.address.locality.code} />
+        <KeyNote keyValue={t("CORE_COMMON_PINCODE")} note={application.address.pincode} />
+        <KeyNote keyValue={t("ES_NEW_APPLICATION_STREET_NAME")} note={application.address.street} />
+        <KeyNote keyValue={t("ES_NEW_APPLICATION_DOOR_NO")} note={application.address.door} />
+        <KeyNote keyValue={t("CS_ADDCOMPLAINT_PROPERTY_LANDMARK")} note={application.address.landmark} />
+        <KeyNote keyValue={t("CS_CHECK_PIT_TYPE")} note={application.pitDetail.type} />
         <KeyNote
           keyValue={t("CS_APPLICATION_DETAILS_PIT_SIZE")}
-          note={`${application.pitDetail.length}m * ${application.pitDetail.width}m * ${application.pitDetail.height}m`}
+          note={`${!!application.pitDetail.length ? application.pitDetail.length + "m " : ""}${
+            !!application.pitDetail.width ? "x " + application.pitDetail.width + "m " : ""
+          }${!!application.pitDetail.height ? "x " + application.pitDetail.height + "m " : ""}${
+            !!application.pitDetail.diameter ? "x" + application.pitDetail.diameter + "m" : ""
+          }`}
         />
-        <KeyNote keyValue={t("CS_APPLICATION_DETAILS_NO_OF_TRIPS")} note={application.noOfTrips} />
-        <KeyNote keyValue={t("CS_APPLICATION_DETAILS_DESLUDGING_CHARGES")} note={application.desuldgingCharges || "NA"} />
+        {/* <KeyNote keyValue={t("CS_APPLICATION_DETAILS_NO_OF_TRIPS")} note={application.noOfTrips} /> */}
+        {/* <KeyNote keyValue={t("CS_APPLICATION_DETAILS_DESLUDGING_CHARGES")} note={application.desuldgingCharges || "NA"} /> */}
         {application.applicationStatus === "PENDING_APPL_FEE_PAYMENT" && (
           <Link to={`/digit-ui/citizen/payment/collect/FSM.TRIP_CHARGES/${application.applicationNo}`}>
             <SubmitBar label={t("CS_APPLICATION_DETAILS_MAKE_PAYMENT")} />

e529768369300c7263e394db873595572ae64214
Abhinav Kushwaha <abhinav.kushwaha@egovernments.org>
2021-01-29 00:34:40 +0530

    t in pdf

--- packages/libraries/src/utils/pdf.js
+++ packages/libraries/src/utils/pdf.js
@@ -7,8 +7,6 @@ import { toDataURL } from "./toDataURL";
 const jsPdfGenerator = async ({ logo, name, email, phoneNumber, heading, details, t = (text) => text }) => {
   const base64Image = await toDataURL(logo);
 
-  const translate = window?.i18next?.t || t;
-
   const dd = {
     pageMargins: [40, 80, 40, 30],
     header: {
@@ -56,7 +54,7 @@ const jsPdfGenerator = async ({ logo, name, email, phoneNumber, heading, details
       },
       ...createContent(details, phoneNumber),
       {
-        text: translate("PDF_SYSTEM_GENERATED_ACKNOWLEDGEMENT"),
+        text: t("PDF_SYSTEM_GENERATED_ACKNOWLEDGEMENT"),
         fontSize: 11,
         color: "#6f777c",
         margin: [-25, 32],

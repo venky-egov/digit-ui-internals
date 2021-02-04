50f22a9409f8ca5593d0699350985a71fc83d41a
himanshu-egov <himanshu.singh@egovernments.org>
2021-02-04 22:48:01 +0530

    FIX: PTlinks issue

--- packages/modules/pt/src/Module.js
+++ packages/modules/pt/src/Module.js
@@ -1,4 +1,10 @@
+import React from "react";
+
 export const PTModule = () => {
   const moduleCode = "PT";
   console.log(moduleCode, "module integrated");
+};
+
+export const PTLinks = () => {
+  return <React.Fragment></React.Fragment>;
 };

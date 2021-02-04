d2d4837be86125920d4f17c24e8bd36bfced34e6
nikhil24 <nikhil15.1990@gmail.com>
2021-02-04 14:07:23 +0530

    added pt home page links

--- packages/modules/pt/src/Module.js
+++ packages/modules/pt/src/Module.js
@@ -1,4 +1,23 @@
+import { Header, HomeLink } from "@egovernments/digit-ui-react-components";
+import React from "react";
+
+import { useTranslation } from "react-i18next";
+
 export const PTModule = () => {
   const moduleCode = "PT";
   console.log(moduleCode, "module integrated");
+};
+
+export const PTLinks = ({ matchPath }) => {
+  const { t } = useTranslation();
+
+  return (
+    <React.Fragment>
+      {/* TODO: change */}
+      <Header>{t("CS_HOME_QUICK_PAY")}</Header>
+      <HomeLink to={`${matchPath}/property-tax`}>{t("CS_HOME_PT")}</HomeLink>
+      <HomeLink to={`${matchPath}/tl-renewal`}>{t("CS_HOME_TRADE_LICENCE_RENEWAL")}</HomeLink>
+      <HomeLink to={`${matchPath}/water-bill`}>{t("CS_HOME_WATER_BILL")}</HomeLink>
+    </React.Fragment>
+  );
 };

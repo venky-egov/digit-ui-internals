8af3f1bd4a80506d349716e06869b595a61dcb1f
himanshu-egov <himanshu.singh@egovernments.org>
2021-02-05 01:13:29 +0530

    added citizen search results

--- packages/modules/pt/src/pages/citizen/index.js
+++ packages/modules/pt/src/pages/citizen/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useRouteMatch, Switch, useLocation } from "react-router-dom";
 import { AppContainer, BackButton, PrivateRoute } from "@egovernments/digit-ui-react-components";
 import SearchPropertyComponent from "./SearchProperty";
+import SearchResultsComponent from "./SearchResults";
 
 const App = () => {
   const { path, url, ...match } = useRouteMatch();
@@ -13,6 +14,7 @@ const App = () => {
       <AppContainer style={mobileView ? { width: "87%" } : null}>
         <BackButton>Back</BackButton>
         <PrivateRoute path={`${path}/property/search`} component={SearchPropertyComponent} />
+        <PrivateRoute path={`${path}/property/search-results`} component={SearchResultsComponent} />
       </AppContainer>
     </Switch>
   );

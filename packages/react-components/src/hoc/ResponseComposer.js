d1ad1f378845a653c9f57423097804f031085edb
himanshu-egov <himanshu.singh@egovernments.org>
2021-02-08 20:56:56 +0530

    fix response composer

--- packages/react-components/src/hoc/ResponseComposer.js
+++ packages/react-components/src/hoc/ResponseComposer.js
@@ -11,12 +11,13 @@ const ResponseComposer = ({ data, template, actionButtonLabel }) => {
   console.log({ data, template, actionButtonLabel });
   return (
     <div>
-      {data.map((result) => {
+      {data.map((result,i) => {
         return (
-          <Card style={{ boxShadow: "none" }}>
-            {template.map((field) => {
+          <Card  key={i} style={{ boxShadow: "none" }}>
+            {template.map((field, j) => {
               return (
                 <KeyNote
+                  key={i+''+j} 
                   keyValue={t(field.label)}
                   note={field.notePrefix ? field.notePrefix + result[field.key] : result[field.key]}
                   noteStyle={field.noteStyle}

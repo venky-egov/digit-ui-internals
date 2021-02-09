aed0d8ce406322a45cb107da7977e0048ee23465
himanshu-egov <himanshu.singh@egovernments.org>
2021-02-09 23:12:11 +0530

    added params in url from search property

--- packages/modules/pt/src/pages/citizen/SearchResults/searchResults.js
+++ packages/modules/pt/src/pages/citizen/SearchResults/searchResults.js
@@ -8,17 +8,19 @@ const PropertySearchResults = ({ template, header, actionButtonLabel, t }) => {
 
   const result = Digit.Hooks.pt.usePropertySearch({ tenantId: "pb", filters: { mobileNumber, propertyIds, oldpropertyids } });
   console.log({ property: result });
-  const searchResults = result?.data?.Properties?.map( property => {
-    let addr = property?.address || {}
+  const searchResults = result?.data?.Properties?.map((property) => {
+    let addr = property?.address || {};
 
-   return { 
+    return {
       property_id: property?.propertyId,
-      owner_name:(property?.owners || [])[0]?.name,
-      property_address: [(addr?.doorNo || ''), (addr?.buildingName || ''), (addr?.street || ''), (addr?.locality?.name || ''), (addr?.city || '') ].filter(a => a).join(', '),
+      owner_name: (property?.owners || [])[0]?.name,
+      property_address: [addr.doorNo || "", addr.buildingName || "", addr.street || "", addr.locality?.name || "", addr.city || ""]
+        .filter((a) => a)
+        .join(", "),
       total_due: "124",
-      bil_due__date: "25-01-2022"
-    }
-  })
+      bil_due__date: "25-01-2022",
+    };
+  });
 
   return (
     <div>

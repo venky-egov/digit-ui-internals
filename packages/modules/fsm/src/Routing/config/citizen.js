e531ccfc34d93044adb3926abfa255f400fa39ea
saurabh-egov <saurabh.s@egovernments.org>
2021-02-12 13:11:15 +0530

    added basic structure for config

---
+++ packages/modules/fsm/src/Routing/config/citizen.js
@@ -0,0 +1,22 @@
+const config = [
+  {
+    path: "new-application",
+    component: "FileComplaint",
+  },
+  {
+    path: "my-applications",
+    component: "MyApplications",
+  },
+  {
+    path: "application-details",
+    component: "ApplicationDetails",
+  },
+  {
+    path: "rate",
+    component: "SelectRating",
+  },
+  {
+    path: "response",
+    component: "Response",
+  },
+];
---
+++ packages/modules/fsm/src/Routing/config/employee.js
@@ -0,0 +1,46 @@
+const config = [
+  {
+    defaultPath: "",
+    component: "FSMLinks",
+  },
+  {
+    path: "inbox",
+    component: "Inbox",
+  },
+  {
+    path: "new-application",
+    component: "NewApplication",
+  },
+  {
+    path: "modify-application",
+    component: "EditApplication",
+  },
+  {
+    path: "application-details",
+    component: "EmployeeApplicationDetails",
+  },
+  {
+    path: "fstp-operator-details",
+    component: "FstpOperatorDetails",
+  },
+  {
+    path: "response",
+    component: "Response",
+  },
+  {
+    path: "collect-payment",
+    component: "CollectPayment",
+  },
+  {
+    path: "application-audit",
+    component: "ApplicationAudit",
+  },
+  {
+    path: "search",
+    component: "SearchApplication",
+  },
+  {
+    path: "mark-for-disposal",
+    component: "MarkForDisposal",
+  },
+];

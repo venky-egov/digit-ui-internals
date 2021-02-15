a8cce019af1716900982462b81ece794f07461f6
harit-egov <harit.saxena@egovernments.org>
2021-02-15 15:53:40 +0530

    merged

--- packages/libraries/src/hooks/userSearch.js
+++ packages/libraries/src/hooks/userSearch.js
@@ -3,6 +3,6 @@ import { UserService } from "../services/elements/User";
 
 export const useUserSearch = (tenantId, data, filters, options = {}) => {
   const client = useQueryClient();
-  const queryData = useQuery(["USER_SEARCH", filters], () => UserService.userSearch(tenantId, data, filters), options);
-  return { ...queryData, revalidate: () => client.invalidateQueries(["USER_SEARCH", filters]) };
+  const queryData = useQuery(["USER_SEARCH", filters, data], () => UserService.userSearch(tenantId, data, filters), options);
+  return { ...queryData, revalidate: () => client.invalidateQueries(["USER_SEARCH", filters, data]) };
 };

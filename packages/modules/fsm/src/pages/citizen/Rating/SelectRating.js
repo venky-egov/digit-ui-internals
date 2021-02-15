8074ac2f663ced03afa7ed0945dfb1c7523ab84b
ayan-egov <ayan.majumder@egovernments.org>
2021-02-15 12:34:32 +0530

    change useSearchAll to useSearch

--- packages/modules/fsm/src/pages/citizen/Rating/SelectRating.js
+++ packages/modules/fsm/src/pages/citizen/Rating/SelectRating.js
@@ -8,7 +8,7 @@ const SelectRating = ({ parentRoute }) => {
   const tenantId = Digit.ULBService.getCurrentTenantId();
   const history = useHistory();
   let { id: applicationNos } = useParams();
-  const { isError, error, data: application } = Digit.Hooks.fsm.useSearchAll(tenantId, { applicationNos });
+  const { isError, error, data: application } = Digit.Hooks.fsm.useSearch(tenantId, { applicationNos });
   const { isLoading, data: checklistData } = Digit.Hooks.fsm.useMDMS(tenantId, "FSM", "Checklist");
   const mutation = Digit.Hooks.fsm.useApplicationUpdate(tenantId);
   const [answers, setAnswers] = useState({});
@@ -24,8 +24,7 @@ const SelectRating = ({ parentRoute }) => {
       return acc;
     }, []);
 
-    const [applicationData] = application;
-    applicationData.additionalDetails = {
+    application.additionalDetails = {
       CHECKLIST: checklist,
     };
     // const requestBody = {
@@ -39,7 +38,7 @@ const SelectRating = ({ parentRoute }) => {
     // };
 
     history.push("/digit-ui/employee/fsm/response", {
-      applicationData,
+      application,
       key: "update",
       action: "SUBMIT_FEEDBACK",
     });
@@ -75,23 +74,6 @@ const SelectRating = ({ parentRoute }) => {
         maxRating: 5,
         label: t("CS_FSM_APPLICATION_RATE_TEXT"),
       },
-      // {
-      //   name: "WHAT_WAS_GOOD",
-      //   type: "checkbox",
-      //   label: t("CS_FEEDBACK_WHAT_WAS_GOOD"),
-      //   checkLabels: [t("CS_FEEDBACK_SERVICES"), t("CS_FEEDBACK_RESOLUTION_TIME"), t("CS_FEEDBACK_QUALITY_OF_WORK"), t("CS_FEEDBACK_OTHERS")],
-      // },
-      // {
-      //   name: "WHAT_WAS_BAD",
-      //   type: "checkbox",
-      //   label: t("CS_FEEDBACK_WHAT_WENT_WRONG"),
-      //   checkLabels: [
-      //     t("CS_FEEDBACK_THERE_WAS_SPILLAGE"),
-      //     t("CS_FEEDBACK_OPERATORS_WERE_WITHOUT_SAFETY_GEARS"),
-      //     t("CS_FEEDBACK_RESOLUTION_TIME_WAS_LONG"),
-      //     t("CS_FEEDBACK_OTHERS"),
-      //   ],
-      // },
       {
         type: "textarea",
         label: t("CS_COMMON_COMMENTS"),

5c20480dee1b37582f85a67ea711af3a91f95828
vamshikrishnakole-wtt-egov <vamshikrishna.kole@egovernments.org>
2021-02-23 15:23:14 +0530

    formed code

--- packages/modules/pt/src/pages/citizen/Create/Steps/SelectSpecialProofIdentityImages.js
+++ packages/modules/pt/src/pages/citizen/Create/Steps/SelectSpecialProofIdentityImages.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { FormStep, ImageUploadHandler, Loader } from "@egovernments/digit-ui-react-components";
+import React, { useState, useEffect } from "react";
+import { FormStep, ImageUploadHandler, Loader, UploadFile } from "@egovernments/digit-ui-react-components";
 
 const SelectSpecialProofIdentityImages = ({ t, config, onSelect, onSkip, value }) => {
   // const __initImages = Digit.SessionStorage.get("PGR_CREATE_IMAGES");
@@ -27,9 +27,53 @@ const SelectSpecialProofIdentityImages = ({ t, config, onSelect, onSkip, value }
     onSelect({ uploadedImages });
   };
 
+
+  const [file, setFile] = useState(null);
+  const [uploadedFile, setUploadedFile] = useState(null);
+  const [error, setError] = useState(null);
+  const cityDetails = Digit.ULBService.getCurrentUlb();
+
+
+  function selectfile(e) {
+    setFile(e.target.files[0]);
+  }
+  useEffect(() => {
+    (async () => {
+      setError(null);
+      if (file) {
+        if (file.size >= 5242880) {
+          setError(t("CS_MAXIMUM_UPLOAD_SIZE_EXCEEDED"));
+        } else {
+          try {
+            // TODO: change module in file storage
+            const response = await Digit.UploadServices.Filestorage("property-upload", file, "pb");
+            if (response?.data?.files?.length > 0) {
+              setUploadedFile(response?.data?.files[0]?.fileStoreId);
+            } else {
+              setError(t("CS_FILE_UPLOAD_ERROR"));
+            }
+          } catch (err) {
+            console.error("Modal -> err ", err);
+            setError(t("CS_FILE_UPLOAD_ERROR"));
+          }
+        }
+      }
+    })();
+  }, [file]);
+
+
+
   return (
     <FormStep config={config} onSelect={handleSubmit} onSkip={onSkip} t={t}>
-      <ImageUploadHandler tenantId="pb.amritsar" uploadedImages={uploadedImages} onPhotoChange={handleUpload} />
+      {/* <ImageUploadHandler tenantId="pb.amritsar" uploadedImages={uploadedImages} onPhotoChange={handleUpload} /> */}
+      <UploadFile
+          accept=".jpg"
+          onUpload={selectfile}
+          onDelete={() => {
+            setUploadedFile(null);
+          }}
+          message={uploadedFile ? `1 ${t(`CS_ACTION_FILEUPLOADED`)}` : t(`CS_ACTION_NO_FILEUPLOADED`)}
+        />
     </FormStep>
   );
 };

e236b0167e1bb1fc0fe0623f63d9e959e6de29ce
Abhinav Kushwaha <abhinav.kushwaha@egovernments.org>
2021-02-26 08:18:16 +0530

    slum name localization fix

--- packages/modules/fsm/src/pageComponents/SelectSlumName.js
+++ packages/modules/fsm/src/pageComponents/SelectSlumName.js
@@ -81,11 +81,11 @@ const SelectSlumName = ({ config, onSelect, t, userType, formData }) => {
         {t("ES_NEW_APPLICATION_SLUM_NAME")}
         {config.isMandatory ? " * " : null}
       </CardLabel>
-      <Dropdown option={slumMenu} style={{ width: "50%" }} optionKey="i18nKey" id="slum" selected={slum} select={selectSlum} />
+      <Dropdown t={t} option={slumMenu} style={{ width: "50%" }} optionKey="i18nKey" id="slum" selected={slum} select={selectSlum} />
     </LabelFieldPair>
   ) : (
     <FormStep t={t} config={config} onSelect={goNext} onSkip={onSkip}>
-      <Dropdown option={slumMenu} optionKey="i18nKey" id="i18nKey" selected={slum} select={setSlum} />
+      <Dropdown t={t} option={slumMenu} optionKey="i18nKey" id="i18nKey" selected={slum} select={setSlum} />
     </FormStep>
   );
 };

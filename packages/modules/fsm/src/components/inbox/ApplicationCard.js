9e888ee9e57c4a3f590a417dede0ef58b62f4856
harit-egov <harit.saxena@egovernments.org>
2021-03-02 21:09:01 +0530

    mobile issues fixed

--- packages/modules/fsm/src/components/inbox/ApplicationCard.js
+++ packages/modules/fsm/src/components/inbox/ApplicationCard.js
@@ -19,41 +19,40 @@ export const ApplicationCard = ({
   searchFields,
   linkPrefix,
 }) => {
-  debugger;
   const [popup, setPopup] = useState(false);
-  const [selectedComponent, setSelectedComponent] = useState(null);
+
   const [params, setParams] = useState(searchParams);
+  const [type, setType] = useState("");
 
   const selectParams = (param) => {
     setParams((o) => ({ ...o, ...param }));
   };
 
   const onSearchPara = (param) => {
-    console.log(param);
     onFilterChange(params);
-    handlePopupClose();
+    setType("");
+    setPopup(false);
   };
+
+  useEffect(() => {
+    console.log("params in application card", params);
+  }, [params]);
+
+  useEffect(() => {
+    if (type) setPopup(true);
+  }, [type]);
 
   const DSO = Digit.UserService.hasAccess("FSM_DSO") || false;
 
   const handlePopupClose = () => {
     setPopup(false);
-    setSelectedComponent(null);
+    setType("");
+    setParams(searchParams);
   };
 
-  const handlePopupAction = (type) => {
-    console.log("option");
+  const handlePopupAction = () => {
     if (type === "SEARCH") {
-      setSelectedComponent(
-        <SearchApplication
-          type="mobile"
-          onClose={handlePopupClose}
-          onSearch={onSearch}
-          isFstpOperator={isFstpOperator}
-          searchParams={searchParams}
-          searchFields={searchFields}
-        />
-      );
+      setSelectedComponent();
     } else if (type === "FILTER") {
       setSelectedComponent(
         <Filter onFilterChange={selectParams} onClose={handlePopupClose} onSearch={onSearchPara} type="mobile" searchParams={params} />
@@ -85,7 +84,7 @@ export const ApplicationCard = ({
     result = (
       <DetailsCard
         data={data}
-        serviceRequestIdKey={serviceRequestIdKey}
+        serviceRequestIdKey={"Application No"}
         linkPrefix={linkPrefix ? linkPrefix : DSO ? "/digit-ui/employee/fsm/application-details/" : "/digit-ui/employee/fsm/"}
       />
     );
@@ -94,14 +93,55 @@ export const ApplicationCard = ({
   return (
     <React.Fragment>
       <div className="searchBox">
-        {onSearch && <SearchAction text="SEARCH" handleActionClick={() => handlePopupAction("SEARCH")} />}
-        {onFilterChange && <FilterAction text="FILTER" handleActionClick={() => handlePopupAction("FILTER")} />}
-        <FilterAction text="SORT" handleActionClick={() => handlePopupAction("SORT")} />
+        {onSearch && (
+          <SearchAction
+            text="SEARCH"
+            handleActionClick={() => {
+              setType("SEARCH");
+              setPopup(true);
+            }}
+          />
+        )}
+        {onFilterChange && (
+          <FilterAction
+            text="FILTER"
+            handleActionClick={() => {
+              setType("FILTER");
+              setPopup(true);
+            }}
+          />
+        )}
+        <FilterAction
+          text="SORT"
+          handleActionClick={() => {
+            setType("SORT");
+            setPopup(true);
+          }}
+        />
       </div>
       {result}
       {popup && (
         <PopUp>
-          <div className="popup-module">{selectedComponent}</div>
+          {type === "FILTER" && (
+            <div className="popup-module">
+              {<Filter onFilterChange={selectParams} onClose={handlePopupClose} onSearch={onSearchPara} type="mobile" searchParams={params} />}
+            </div>
+          )}
+          {type === "SORT" && (
+            <div className="popup-module">
+              {<SortBy type="mobile" searchParams={searchParams} onClose={handlePopupClose} type="mobile" onSort={selectParams} />}
+            </div>
+          )}
+          {type === "Search" && (
+            <SearchApplication
+              type="mobile"
+              onClose={handlePopupClose}
+              onSearch={onSearch}
+              isFstpOperator={isFstpOperator}
+              searchParams={searchParams}
+              searchFields={searchFields}
+            />
+          )}
         </PopUp>
       )}
     </React.Fragment>

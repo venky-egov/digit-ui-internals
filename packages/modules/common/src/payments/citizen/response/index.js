f61f0f942f6a215a8bc43113a414b3082ec459ad
Abhinav Kushwaha <abhinav.kushwaha@egovernments.org>
2021-03-05 16:24:56 +0530

    import useeffect

--- packages/modules/common/src/payments/citizen/response/index.js
+++ packages/modules/common/src/payments/citizen/response/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Banner, Card, CardText, Loader, Row, StatusTable, SubmitBar } from "@egovernments/digit-ui-react-components";
 import { useHistory, useParams, Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";

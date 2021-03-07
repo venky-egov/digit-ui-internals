b5edaef84ff34aa84d2f1334384f62ce95e1549f
harit-egov <harit.saxena@egovernments.org>
2021-03-07 19:56:27 +0530

    correction-card

--- packages/react-components/src/atoms/Card.js
+++ packages/react-components/src/atoms/Card.js
@@ -4,11 +4,12 @@ import { useLocation } from "react-router-dom";
 const Card = ({ className, onClick, style, children, ...props }) => {
   const { pathname } = useLocation();
   const classname = Digit.Hooks.fsm.useRouteSubscription(pathname);
-  const isEmployee = classname === "employee";
-  const userType = Digit.SessionStorage.get("userType");
+  const info = Digit.UserService.getUser()?.info;
+  const userType = info.type;
+  const isEmployee = classname === "employee" || userType === "EMPLOYEE";
   return (
     // <div className={className ? className : userType === "employee" ? "employeeCard" : "card"} onClick={onClick} style={style} {...props}>
-    <div className={className ? className : isEmployee ? "employeeCard" : "card"} onClick={onClick} style={style} {...props}>
+    <div className={isEmployee ? "employeeCard" : "card"} onClick={onClick} style={style} {...props}>
       {children}
     </div>
   );

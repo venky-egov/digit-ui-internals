545f7abf0049f2de59c120c0738f4657ab344c8e
Mahendra <mahendra.choudhary@egovernments.org>
2021-03-08 12:04:12 +0530

    refactor

--- packages/modules/fsm/src/pages/employee/ApplicationDetails/config/CompleteApplication.js
+++ packages/modules/fsm/src/pages/employee/ApplicationDetails/config/CompleteApplication.js
@@ -1,41 +1,39 @@
 import React from "react";
 
-export const configCompleteApplication = ({ t, vehicle, applicationCreatedTime = 0, action }) => {
-  return {
-    label: {
-      heading: `ES_FSM_ACTION_TITLE_${action}`,
-      submit: `CS_COMMON_${action}`,
-      cancel: "CS_COMMON_CANCEL",
+export const configCompleteApplication = ({ t, vehicle, applicationCreatedTime = 0, action }) => ({
+  label: {
+    heading: `ES_FSM_ACTION_TITLE_${action}`,
+    submit: `CS_COMMON_${action}`,
+    cancel: "CS_COMMON_CANCEL",
+  },
+  form: [
+    {
+      body: [
+        {
+          label: t("ES_FSM_ACTION_DESLUGED_DATE_LABEL"),
+          isMandatory: true,
+          type: "date",
+          populators: {
+            name: "desluged",
+            min: new Date(applicationCreatedTime).toISOString().split("T")[0],
+            max: new Date().toISOString().split("T")[0],
+            defaultValue: new Date().toISOString().split("T")[0],
+          },
+        },
+        {
+          label: t("ES_FSM_ACTION_WASTE_VOLUME_LABEL"),
+          type: "text",
+          isMandatory: true,
+          populators: {
+            name: "wasteCollected",
+            validation: {
+              required: true,
+              validate: (value) => value <= vehicle.capacity,
+            },
+            defaultValue: vehicle?.capacity,
+          },
+        },
+      ],
     },
-    form: [
-      {
-        body: [
-          {
-            label: t("ES_FSM_ACTION_DESLUGED_DATE_LABEL"),
-            isMandatory: true,
-            type: "date",
-            populators: {
-              name: "desluged",
-              min: new Date(applicationCreatedTime).toISOString().split("T")[0],
-              max: new Date().toISOString().split("T")[0],
-              defaultValue: new Date().toISOString().split("T")[0],
-            },
-          },
-          {
-            label: t("ES_FSM_ACTION_WASTE_VOLUME_LABEL"),
-            type: "text",
-            isMandatory: true,
-            populators: {
-              name: "wasteCollected",
-              validation: {
-                required: true,
-                validate: (value) => value <= vehicle.capacity,
-              },
-              defaultValue: vehicle?.capacity,
-            },
-          },
-        ],
-      },
-    ],
-  };
-};
+  ],
+});

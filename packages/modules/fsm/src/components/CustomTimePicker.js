b192e2f3f9e4d8087379934b88159118acb96392
ayan-egov <ayan.majumder@egovernments.org>
2021-03-09 08:27:50 -0300

    Fixed time picker leading zero issue

--- packages/modules/fsm/src/components/CustomTimePicker.js
+++ packages/modules/fsm/src/components/CustomTimePicker.js
@@ -4,6 +4,13 @@ import { TextInput } from "@egovernments/digit-ui-react-components";
 
 const CustomTimePicker = ({ name, value, onChange }) => {
   const timeFormat = (new Date()).toLocaleTimeString();
+  const onTimeKeyDown = (e) => {
+    if((e.keyCode == 48 || e.keyCode == 96) && e.target.value){
+      e.preventDefault();
+    } else if(((e.keyCode > 48 && e.keyCode <= 57) || (e.keyCode > 96 && e.keyCode <= 105)) && e.target.value === '0'){
+      e.preventDefault();
+    }
+  }
   if (timeFormat.includes('AM') || timeFormat.includes('PM')) {
     return (
       <TextInput name={name} type="time" value={value} onChange={event => onChange(event.target.value)} style={{ width: "100%", maxWidth: "200px" }} />
@@ -11,7 +18,7 @@ const CustomTimePicker = ({ name, value, onChange }) => {
   }
 
   return (
-    <TimePicker name={name} onChange={onChange} value={value} locale="en-US" disableClock={false} clearIcon={null} />
+    <TimePicker name={name} onChange={onChange} value={value} locale="en-US" disableClock={false} clearIcon={null} maxLength={2} onKeyDown={onTimeKeyDown} />
   )
 }
 

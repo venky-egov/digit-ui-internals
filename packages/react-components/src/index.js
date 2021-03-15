9f2a64baff3088b00608e3600e5657284fc92b95
tulika kundu <tulika.kundu@egovernments.org>
2021-03-15 13:57:21 +0530

    fix for error due to removal of PropertyTaxRegistration

--- packages/react-components/src/index.js
+++ packages/react-components/src/index.js
@@ -78,7 +78,6 @@ import FilterAction from "./molecules/FilterAction";
 import PitDimension from "./molecules/PitDimension";
 import RadioOrSelect from "./molecules/RadioOrSelect";
 import DashboardBox from "./molecules/DashboardBox";
-import PropertyTaxRegistration from "./molecules/PropertyTaxRegistration";
 
 import { FormComposer } from "./hoc/FormComposer";
 import ResponseComposer from "./hoc/ResponseComposer";
@@ -178,7 +177,6 @@ export {
   ApplyFilterBar,
   RemoveableTag,
   RadioOrSelect,
-  PropertyTaxRegistration,
   // hoc
   FormComposer,
   ResponseComposer,

c91d81d58e2a92cc3efd02f6039186e55548de1d
Priyanka <priyanka@egovernments.org>
2021-03-17 10:11:09 +0530

    changes for Property Details flow

---
+++ packages/modules/pt/src/pageComponents/GroundFloorDetails.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { FormStep } from "@egovernments/digit-ui-react-components";
+
+const GroundFloorDetails = ({ t, config, onSelect, value, userType, formData }) => {
+  const onSkip = () => onSelect();
+
+  const inputs = [
+    {
+      label: "Plot Size(sq.yd)",
+      type: "text",
+      name: "PlotSize",
+      validation: {
+        pattern: "/^[ws]{1,256}$/",
+      },
+      error: "CORE_COMMON_PLOTSIZE_INVALID",
+    },
+    {
+      label: "Built Up Area(sq.yd)",
+      type: "text",
+      name: "BuiltUpArea",
+      validation: {
+        pattern: "/^[w]([w/,s])*$/",
+      },
+      error: "CORE_COMMON_AREA_INVALID",
+    },
+  ];
+
+  return (
+    <FormStep
+      config={{ ...config, inputs }}
+      _defaultValues={{ PlotSize: value?.PlotSize, BuiltUpArea: value?.BuiltUpArea }}
+      onSelect={(data) => onSelect(data)}
+      onSkip={onSkip}
+      t={t}
+    />
+  );
+};
+
+export default GroundFloorDetails;
---
+++ packages/modules/pt/src/pageComponents/PropertyUsageType.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+import { FormStep, TypeSelectCard, RadioButtons } from "@egovernments/digit-ui-react-components";
+
+const PropertyUsageType = ({ t, config, onSelect, userType, formData }) => {
+  const [PropertyPurpose, setPropertyPurpose] = useState(formData?.PropertyUsageType);
+  //   const { data: Menu, isLoading } = Digit.Hooks.pt.usePropertyMDMS(stateId, "PropertyTax", "OccupancyType");
+
+  const menu = [
+    {
+      i18nKey: "PROPERTYTAX_BILLING_SLAB_INSTITUTIONAL",
+    },
+    {
+      i18nKey: "PROPERTYTAX_BILLING_SLAB_INDUSTRIAL",
+    },
+    {
+      i18nKey: "PROPERTYTAX_BILLING_SLAB_COMMERCIAL",
+    },
+  ];
+
+  const onSkip = () => onSelect();
+
+  useEffect(() => {
+    if (userType !== "employee" && formData?.IsResidential?.ResidentialType?.code === false) onSelect(config.key, {}, true);
+  }, [formData?.IsResidential?.ResidentialType]);
+
+  function selectPropertyPurpose(value) {
+    setPropertyPurpose(value);
+  }
+  debugger;
+
+  function goNext() {
+    onSelect(config.key, PropertyPurpose);
+    // onSelect(config.key,ResidentialType, false, index);
+  }
+  return (
+    <FormStep t={t} config={config} onSelect={goNext} onSkip={onSkip} isDisabled={!PropertyPurpose}>
+      <RadioButtons
+        t={t}
+        optionsKey="i18nKey"
+        isMandatory={config.isMandatory}
+        options={menu}
+        selectedOption={PropertyPurpose}
+        onSelect={selectPropertyPurpose}
+      />
+    </FormStep>
+  );
+};
+
+export default PropertyUsageType;

fa7de72579818670241e734ea97e0e5007fca683
harit-egov <harit.saxena@egovernments.org>
2021-03-24 19:37:40 +0530

    LIN-126

--- packages/modules/pt/src/pages/citizen/SearchProperty/searchProperty.js
+++ packages/modules/pt/src/pages/citizen/SearchProperty/searchProperty.js
@@ -12,7 +12,7 @@ const SearchProperty = ({ config: propsConfig, t }) => {
       return alert("Provide at least one parameter");
     } else {
       history.push(
-        `/digit-ui/citizen/pt/property/search-results?mobileNumber=${data.mobileNumber}&=propertyIds=${data.propertyId}&oldPropertyIds=${data.oldPropertyId}`
+        `/digit-ui/citizen/pt/property/search-results?mobileNumber=${data.mobileNumber}&propertyIds=${data.propertyId}&oldPropertyIds=${data.oldPropertyId}`
       );
     }
   };

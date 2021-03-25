767af3b1f182c43e3cba14f30784b250bb2961eb
ssaurabh <saurabh.s@egovernments.org>
2021-03-25 19:41:08 +0530

    changed reciept tenant

--- packages/modules/common/src/payments/citizen/response/index.js
+++ packages/modules/common/src/payments/citizen/response/index.js
@@ -76,9 +76,10 @@ export const SuccessfulPayment = (props) => {
     if (printing) return;
     setPrinting(true);
     const tenantId = paymentData?.tenantId;
+    const state = tenantId?.split(".")[0];
     let response = { filestoreIds: [payments.Payments[0]?.fileStoreId] };
     if (!paymentData?.fileStoreId) {
-      response = await Digit.PaymentService.generatePdf(tenantId, { Payments: payments.Payments });
+      response = await Digit.PaymentService.generatePdf(state, { Payments: payments.Payments });
     }
     const fileStore = await Digit.PaymentService.printReciept(tenantId, { fileStoreIds: response.filestoreIds[0] });
     if (fileStore && fileStore[response.filestoreIds[0]]) {
